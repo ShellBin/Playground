712472c7add3212ae836f6161e69a554ad0b6404
ShellBin <maoxiaobing@live.com>
2020-08-13 19:35:21 +0800

    RandomMusic update

--- localhost_page/randomMusic/index.php
+++ localhost_page/randomMusic/index.php
@@ -32,7 +32,7 @@ function getAllFiles () { // MP3 files ls
 
 function getRandomMusic() {
     $dir = './';
-    $sitePath = str_replace("index.php?i=random","",'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
+    $sitePath = str_replace("index.php","",'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF']);
     $arr = [];
 
     if (false != ($handle = opendir ($dir))) {
@@ -45,22 +45,36 @@ function getRandomMusic() {
         closedir($handle);
         $key = array_rand($arr,1);
     }
-    return $sitePath . $arr[$key];
+    return $sitePath .  rawurlencode($arr[$key]);
 }
 
 if ($str === "random") {
     header("Location:". getRandomMusic());
-    // echo getRandomMusic();
     exit();
-} elseif($str === "ls") {
+} elseif ($str === "legacyRandom.mp3") {
+    $file = getRandomMusic();
+    if(ob_get_contents()) ob_end_clean();
+    header("Content-Type: audio/mpeg");
+    Header("Accept-Ranges: bytes");
+    header("Content-Length: " . filesize($file));
+    header("Content-disposition: attachment; filename=\"" . basename($file) . "\"");
+    ob_clean();
+    flush();
+    readfile($file);
+    exit();
+} elseif ($str === "ls") {
     echo "<h1>All mp3 files in ShellBin's PC</h1>";
     echo getAllFiles ();
+    exit();
 } elseif ($str === null) {
     $sitePath = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
     echo "<p>Get a random mp3 file form ShellBin's PC，there are " . getFileCounts() . " mp3 files here！</p>";
     echo "<p>You can use GET parameter \"<b><a href='" . $sitePath ."?i=random'>?i=random</a></b>\" to redirect to a random mp3 file URL</p>";
+    echo "<p>for some Devices that don't support redirection (like some embedded library), Can also be used \"<b><a href='" . $sitePath ."?i=legacyRandom.mp3'>?i=legacyRandom.mp3</a></b>\" to get mp3 binary</p>";
     echo "<p>or use \"<b><a href='" . $sitePath ."?i=ls'>?i=ls</a></b>\" to review all mp3 files</p>";
     echo "<P>The copyright of all .mp3 file belongs to NetEase.Inc</P>";
+    exit();
 } else {
     echo "Illegal parameter！";
+    exit();
 }
